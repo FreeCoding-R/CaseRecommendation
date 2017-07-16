cf67f1e641de254814cc687c7c19b63cd6744bec
151250212 <151250212@smail.nju.edu.cn>
2017-07-16 16:39:49 +0800

    配置路由

--- src/main/java/freecoding/web/ctrl/routes/User.java
+++ src/main/java/freecoding/web/ctrl/routes/User.java
@@ -2,24 +2,21 @@ package freecoding.web.ctrl.routes;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
 /**
- * Created by zjy on 2017/7/15.
+ * Created by zjy on 2017/7/16.
  */
 @EnableAutoConfiguration
-@Controller
-public class Index {
+@RestController
+public class User {
 
-    @RequestMapping(value="/", method= RequestMethod.GET)
-    public String home() {
-        return "views/index.html";
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String user(@RequestParam(value = "name", required = false) String name,
+                       @RequestParam(value = "pwd", required = false) String pwd) {
+        return "Hello " + name;
     }
-
 }
