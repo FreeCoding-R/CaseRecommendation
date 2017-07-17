91e72c13f742a6137d96d2f7f47c3c9d69c2e50f
151250212 <151250212@smail.nju.edu.cn>
2017-07-17 20:05:58 +0800

    添加注释

--- src/main/java/freecoding/util/Command.java
+++ src/main/java/freecoding/util/Command.java
@@ -5,9 +5,17 @@ import java.io.InputStreamReader;
 
 /**
  * Created by zjy on 2017/7/17.
+ * 通过java代码执行控制台命令的工具类
  */
 public class Command {
-    public static void exeCmd(String commandStr) {
+    /**
+     * 执行命令，一定要注意操作系统环境，一般windows下运行执行的是cmd命令，mac和linux下运行是shell命令，
+     * eg.  Command.exeCmd("ping www.baidu.com");
+     * @param commandStr 被执行的命令
+     * @return 控制台的输出
+     * @apiNote 暂时还没处理编码格式的问题，默认按utf-8的格式读取输出
+     */
+    public static String exeCmd(String commandStr) {
         BufferedReader br = null;
         try {
             Process p = Runtime.getRuntime().exec(commandStr);
@@ -18,6 +26,7 @@ public class Command {
                 sb.append(line + "\n");
             }
             System.out.println(sb.toString());
+            return sb.toString();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -32,6 +41,7 @@ public class Command {
                 }
             }
         }
+        return "no output";
     }
 
     public static void main(String[] args) {
