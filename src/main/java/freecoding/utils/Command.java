756eb80b40be3fe7efba4eb901b31031e8de2af5
151250212 <151250212@smail.nju.edu.cn>
2017-07-17 14:46:33 +0800

    添加命令工具

---
+++ src/main/java/freecoding/utils/Command.java
@@ -0,0 +1,42 @@
+package freecoding.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by zjy on 2017/7/17.
+ */
+public class Command {
+    public static void exeCmd(String commandStr) {
+        BufferedReader br = null;
+        try {
+            Process p = Runtime.getRuntime().exec(commandStr);
+            br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = null;
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            System.out.println(sb.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (br != null)
+            {
+                try {
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String commandStr = "ping www.taobao.com";
+        //String commandStr = "ipconfig";
+        Command.exeCmd(commandStr);
+    }
+}
