73533a1f3b96f48ae9fd81e8cac0fa411a6469fc
151250212 <151250212@smail.nju.edu.cn>
2017-07-19 11:42:06 +0800

    完成页脚
    同时解决了2个warnning
    logo的替换

--- src/main/java/freecoding/util/Command.java
+++ src/main/java/freecoding/util/Command.java
@@ -13,7 +13,6 @@ public class Command {
      * eg.  Command.exeCmd("ping www.baidu.com");
      * @param commandStr 被执行的命令
      * @return 控制台的输出
-     * @apiNote 暂时还没处理编码格式的问题，默认按utf-8的格式读取输出
      */
     public static String exeCmd(String commandStr) {
         String output="";
