699c8bd28709c7787d0dbf8cef10d3ae54d4758d
151250212 <151250212@smail.nju.edu.cn>
2017-07-20 11:46:00 +0800

    添加文件缓存

--- src/main/java/freecoding/web/ctrl/routes/Index.java
+++ src/main/java/freecoding/web/ctrl/routes/Index.java
@@ -6,7 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +35,39 @@ public class Index {
     @RequestMapping(value="/case", method= RequestMethod.GET)
     public String toCase() {
         return "case";
+    }
+
+    @RequestMapping(value="/uploadXML", method= RequestMethod.POST)
+    public String uploadXML(Model model,
+                            @RequestParam(value = "file", required = true) MultipartFile file) {
+        String message="";
+        if (file.isEmpty()) {
+            message="文件为空";
+        }else {
+            // 获取文件名
+            message+=("上传的文件名为：" + fileName + "\n");
+            // 获取文件的后缀名
+            String suffixName = fileName.substring(fileName.lastIndexOf("."));
+            message+=("上传的后缀名为：" + suffixName + "\n");
+            // 文件上传后的路径
+            try {
+                // 解决中文问题，liunx下中文路径，图片显示问题
+                String filePath = getClass().getClassLoader().getResource("").toURI().getPath()+"/userFiles/";
+                File dest = new File(filePath + fileName);
+                // 检测是否存在目录
+                if (!dest.getParentFile().exists()) {
+                    dest.getParentFile().mkdirs();
+                }
+                file.transferTo(dest);
+                message+= "上传成功\n";
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                message+= "上传失败\n";
+            }
+        }
+        model.addAttribute("result",message);
+        return "result";
     }
 
     @RequestMapping("/person")
