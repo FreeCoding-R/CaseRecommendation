46ef44fed38dd5e591f084ddcab856e8ecd186c8
151250212 <151250212@smail.nju.edu.cn>
2017-07-25 16:56:48 +0800

    添加登陆控制

--- src/main/java/freecoding/web/ctrl/routes/User.java
+++ src/main/java/freecoding/web/ctrl/routes/User.java
@@ -1,22 +1,63 @@
 package freecoding.web.ctrl.routes;
 
+import freecoding.web.ctrl.security.WebSecurityConfig;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zjy on 2017/7/16.
  */
 @EnableAutoConfiguration
-@RestController
+@Controller
 public class User {
 
     @RequestMapping(value = "/user")
-    public String user(@RequestParam(value = "name", required = false) String name,
-                       @RequestParam(value = "pwd", required = false) String pwd) {
+    public @ResponseBody String user(
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "pwd", required = false) String pwd) {
         return "Hello " + name;
     }
+
+    @RequestMapping(value = "/user/name", method= RequestMethod.GET)
+    public @ResponseBody String userName(@SessionAttribute(WebSecurityConfig.SESSION_KEY) String username, Model model) {
+        return "user: " + username;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/loginPost")
+    public @ResponseBody Map<String, Object> loginPost(String username, String password, HttpSession session) {
+        Map<String, Object> map = new HashMap<>();
+        if (!"123456".equals(password)) {
+            map.put("success", false);
+            map.put("message", "密码错误");
+            return map;
+        }
+
+        // 设置session
+        session.setAttribute(WebSecurityConfig.SESSION_KEY, username);
+
+        map.put("success", true);
+        map.put("message", "登录成功");
+        return map;
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        // 移除session
+        session.removeAttribute(WebSecurityConfig.SESSION_KEY);
+        return "redirect:/login";
+    }
+
 }
---
+++ src/main/java/freecoding/web/ctrl/security/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package freecoding.web.ctrl.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by zjy on 2017/7/25.
+ * 用户访问权限的控制
+ */
+@Configuration
+public class WebSecurityConfig extends WebMvcConfigurerAdapter {
+
+    /**
+     * 登录session key
+     */
+    public final static String SESSION_KEY = "user";
+
+    @Bean
+    public SecurityInterceptor getSecurityInterceptor() {
+        return new SecurityInterceptor();
+    }
+
+    /**
+     * 设置拦截规则
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration addInterceptor = registry.addInterceptor(getSecurityInterceptor());
+
+        // 排除配置,下面的路径不进行权限控制
+        addInterceptor.excludePathPatterns("/error/**");
+        addInterceptor.excludePathPatterns("/login**");
+
+        // 拦截配置，下面的路径进行拦截，判断是否有权限
+        addInterceptor.addPathPatterns("/user/**");
+    }
+
+    private class SecurityInterceptor extends HandlerInterceptorAdapter {
+
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            HttpSession session = request.getSession();
+            if (session.getAttribute(SESSION_KEY) != null)
+                return true;
+
+            // 如果没有权限访问，跳转到登录页面
+            String url = "/login";
+            response.sendRedirect(url);
+            return false;
+        }
+    }
+}
