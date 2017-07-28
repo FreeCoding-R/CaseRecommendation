6261cbbc76ba96ac3c313f8bbf02d21b99d7324e
loickjoingit <151250133@smail.nju.edu.cn>
2017-07-28 09:17:05 +0800

    mongodatatest

--- src/test/java/freecoding/util/MongoDataTest.java
+++ src/test/java/freecoding/util/MongoDataTest.java
@@ -2,11 +2,16 @@ package freecoding.util;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Created by 铠联 on 2017/7/19.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MongoDataTest {
     @Autowired
     private MongodbUtil mongoData;
